3348514adb4d4f2153037ea1ea69b4d96eebefe7
Dirceu Rezende <dirceu.srezende@gmail.com>
2022-08-06 16:42:19 -0300

    chore: add styled-components config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { StyledThemeProvider } from './styles/styled-theme-provider';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StyledThemeProvider>
+      <App />
+    </StyledThemeProvider>
   </React.StrictMode>
 );
